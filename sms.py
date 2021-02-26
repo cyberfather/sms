42b3ce1576109234750cb99140896ae09d0f3068
cyberfather <79603958+cyberfather@users.noreply.github.com>
2021-02-26 10:19:00 -0800

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,5 +1,12 @@
 import requests
 import time
+print("            _                  __       _   _               
+  ___ _   _| |__   ___ _ __   / _| __ _| |_| |__   ___ _ __ 
+ / __| | | | '_ \ / _ \ '__| | |_ / _` | __| '_ \ / _ \ '__|
+| (__| |_| | |_) |  __/ |    |  _| (_| | |_| | | |  __/ |   
+ \___|\__, |_.__/ \___|_|    |_|  \__,_|\__|_| |_|\___|_|   
+      |___/                                                 
+                                   ")
 phnenum = input("Enter The Phone Number : ")
 
 urlsend = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
